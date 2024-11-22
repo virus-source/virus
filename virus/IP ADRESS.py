6f9b1a3e13dd1a80908709ff4546bdcca7064e6c
virus(fan maker :) <gegagedigedagedagogg@gmail.com>
2024-11-22 18:40:34 +0100

    Update IP ADRESS.py

--- virus/IP ADRESS.py
+++ virus/IP ADRESS.py
@@ -2,7 +2,7 @@ import socket
 
 def get_static_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))  # conect to google DNC
+    s.connect(("8.8.8.8", 80))  # conect to google DNS
     ip_address = s.getsockname()[0]
     s.close()
     return ip_address
